38c618aa8cabd35ca017cae292364edd6012b04a
Spoonstriker <82917830+Spoonstriker@users.noreply.github.com>
2024-02-08 13:08:35 +0100

    removed sidebar

--- client/travelapp/src/components/Layout/Layout.jsx
+++ client/travelapp/src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../HeaderFooter/Header';
 import Footer from '../HeaderFooter/Footer';
-import Sidebar from '../HeaderFooter/Sidebar';
+
 
 export default function Layout({content}) {
   return (
