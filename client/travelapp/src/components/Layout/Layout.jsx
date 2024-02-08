4d99091ac975fbf82821e3eefff13f5c3e1b28bd
Spoonstriker <82917830+Spoonstriker@users.noreply.github.com>
2024-02-08 13:07:44 +0100

    merge

--- client/travelapp/src/components/Layout/Layout.jsx
+++ client/travelapp/src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../HeaderFooter/Header';
 import Footer from '../HeaderFooter/Footer';
+import Sidebar from '../HeaderFooter/Sidebar';
 
 export default function Layout({content}) {
   return (
     <div>
-      <Header />
-
+      <Header />  
+      <content/>
       <Footer />
     </div>
   )
