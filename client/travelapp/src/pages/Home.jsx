b1086128543ecb6e9d241fa37672ec8807226deb
Markuv <markuv@stud.ntnu.no>
2024-02-15 14:36:18 +0100

    Fikset feil div brackets

--- client/travelapp/src/pages/Home.jsx
+++ client/travelapp/src/pages/Home.jsx
@@ -10,30 +10,31 @@ const Home = (props) => {
 
     return (
         <div>
-    <div className="mainContainer">
-        <div className={"titleContainer"}>
-            <div>Welcome! HOME</div>
-        </div>
-        <div>
-            This is the home page.
-        </div>
-        <div className={"buttonContainer"}>
-            <ul>
-                <li>
-                    <NavLink to ="/Login"> Log in / Become a new member </NavLink>
-                </li>
-            </ul>
-            {(loggedIn ? <div>
-                Your email address is {email}
-            </div> : <div/>)}
-        </div>
-        
-        <Destinasjonsboks rating ={4} land={"Canada"} tittel={"Amerika"} beskrivelse={"hegfuoashakshhegfuoashaks hhegfuoashakshhegfuoashaks hhegfuoa"}/>
-        <Destinasjonsboks rating ={4} land={"norge"} tittel={"sa"} beskrivelse={"hegfuoegfuoashakshhegfuoas hakshhegfuoashakhhegfuoashaksh"}/>
-        <Destinasjonsboks land={"Canaadfda"} tittel={"Ameadsfrika"} />
-        <Destinasjonsboks land={"Canada"} tittel={"Amerika"}/>
-        <Destinasjonsboks land={"norge"} tittel={"sa"}/>
-        <Destinasjonsboks land={"Canaadfda"} tittel={"Ameadsfrika"}/>
+            <div className="mainContainer">
+                <div className={"titleContainer"}>
+                    <div>Welcome! HOME</div>
+                </div>
+                <div>
+                    This is the home page.
+                </div>
+                <div className={"buttonContainer"}>
+                    <ul>
+                        <li>
+                            <NavLink to ="/Login"> Log in / Become a new member </NavLink>
+                        </li>
+                    </ul>
+                    {(loggedIn ? <div>
+                        Your email address is {email}
+                    </div> : <div/>)}
+                </div>
+                
+                <Destinasjonsboks rating ={4} land={"Canada"} tittel={"Amerika"} beskrivelse={"hegfuoashakshhegfuoashaks hhegfuoashakshhegfuoashaks hhegfuoa"}/>
+                <Destinasjonsboks rating ={4} land={"norge"} tittel={"sa"} beskrivelse={"hegfuoegfuoashakshhegfuoas hakshhegfuoashakhhegfuoashaksh"}/>
+                <Destinasjonsboks land={"Canaadfda"} tittel={"Ameadsfrika"} />
+                <Destinasjonsboks land={"Canada"} tittel={"Amerika"}/>
+                <Destinasjonsboks land={"norge"} tittel={"sa"}/>
+                <Destinasjonsboks land={"Canaadfda"} tittel={"Ameadsfrika"}/>
+            </div>
         </div>
     )
     
