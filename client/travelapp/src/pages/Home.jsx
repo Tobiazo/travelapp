ecd2c7684f26b136f52c9734e2010e4b4b8abe47
Eirik Waatevik Saure <eirikwsa@stud.ntnu.no>
2024-02-20 12:03:51 +0100

    Fikset feil i home

--- client/travelapp/src/pages/Home.jsx
+++ client/travelapp/src/pages/Home.jsx
@@ -63,7 +63,7 @@ const Home = (props) => {
                 <Destinasjonsboks rating ={4} land={"Canada"} tittel={"Paris"} beskrivelse={"Kjærlighetens by. Kjent for god mat og romantisk stemning."}/>
                 <Destinasjonsboks rating ={4} land={"norge"} tittel={"Alicante"} beskrivelse={"Varm ferieby. Kjent for gode strender, med mange norske tursiter"}/>
                 <Destinasjonsboks land={"Toronto"} tittel={"Toronto"} beskrivelse={"Beverenes hjemby. Og mest folkerike byen i canada. Kjent for deres hyggelige tilnærming"} />    
-
+                </div>
             {(loggedIn ? <div>
                 Your email address is {email}
             </div> : <div/>)}
