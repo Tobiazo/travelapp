2b35545d22b783571b6a8d2824f77beae3aa98e5
Eirik Waatevik Saure <eirikwsa@stud.ntnu.no>
2024-02-27 11:58:32 +0100

    Resolve "Lag side for å legge til destinasjon"

--- client/travelapp/src/pages/Upload.jsx
+++ client/travelapp/src/pages/Upload.jsx
@@ -4,12 +4,21 @@ import "../styles/Login.css";
 
 const UploadImage = () => {
   const [file, setFile] = useState(null);
-  const [destinationID, setDestinationID] = useState("");
+  const [destination_name, setDestinationName] = useState("");
+  const [destination_contry, setDestinationCountry] = useState("");
+  const [ShortDescription, setShortDescription] = useState("");
+  const [longDescription, setLongDescription] = useState("");
 
   const submit = async (event) => {
     event.preventDefault();
     const formdata = new FormData();
-    formdata.append("id", destinationID);
+
+    //Formdata for å lage ny destninasjon
+    formdata.append("destination_name", destination_name);
+    formdata.append("destination_contry", destination_contry);
+    formdata.append("ShortDescription", ShortDescription);
+    formdata.append("longDescription", longDescription);
+
     formdata.append("file", file);
     axios
       .post("http://localhost:4000/upload", formdata)
@@ -32,9 +41,24 @@ const UploadImage = () => {
                   accept="image/*"
                 ></input>
                 <input
-                  onChange={(e) => setDestinationID(e.target.value)} //Foreløpig må man legge inn id på destinasjonen
+                  onChange={(e) => setDestinationName(e.target.value)} //Foreløpig må man legge inn id på destinasjonen
                   type="text"
-                  placeholder="ID of destination"
+                  placeholder="Name of destination"
+                ></input>
+                <input
+                  onChange={(e) => setDestinationCountry(e.target.value)} //Foreløpig må man legge inn id på destinasjonen
+                  type="text"
+                  placeholder="Country of destination"
+                ></input>
+                <input
+                  onChange={(e) => setShortDescription(e.target.value)} //Foreløpig må man legge inn id på destinasjonen
+                  type="text"
+                  placeholder="Short description"
+                ></input>
+                <input
+                  onChange={(e) => setLongDescription(e.target.value)} //Foreløpig må man legge inn id på destinasjonen
+                  type="text"
+                  placeholder="Long description"
                 ></input>
                 <button type="submit">Submit</button>
               </form>
