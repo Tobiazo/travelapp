e9b07ff3c14e72427f42c66b517806efec22432f
Spoonstriker <82917830+Spoonstriker@users.noreply.github.com>
2024-02-29 15:38:00 +0100

    fiksa litt css og brukernamn

--- client/travelapp/src/components/HeaderFooter/Button.jsx
+++ client/travelapp/src/components/HeaderFooter/Button.jsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Button(){
 
     const bruker = localStorage.getItem("loggedIn")
 
+    const [username, setUsername] = useState("");
+    //const [user, setUser]  = useState({});
     const LogOut = (e) => {
         e.preventDefault()
         localStorage.setItem("loggedIn", "")
         window.location.reload(false)
-    }
+    };
+    
+
+    useEffect(() => {
+     const fetchUser = async () => {
+        
+            try {
+                const response = await axios.get(`http://localhost:4000/api/users/find/${bruker}`);
+                const userData = response.data;
+                // Extract the username from userData and set it to the username state
+                setUsername(userData.username); // Assuming the username property exists in userData
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            }
+        };
+
+        // Call fetchUser if bruker exists
+       
+            fetchUser();
+         
+
+        
+    }, [username]);
+
+    
 
     return(
         <div className={"buttonContainer"}>
-
-                
-                <div id="links">
+            <div id="links">
                 <NavLink className="float" id="navlinklogginn" to ="/Login">{!bruker && "🔑 Logg inn"}  </NavLink>
                 
-                <NavLink className="float" id="navlinklogginn" to ="/Login"  onClick={LogOut}> {bruker && "Logg ut"}  </NavLink>
+                <a className="float" id="navlinklogginn"> {username}  </a>
             </div>
-
         </div>
-    )}
-
+    ) }
+     
     export default Button;
+   
--- client/travelapp/src/components/HeaderFooter/DropdownProfile.jsx
+++ client/travelapp/src/components/HeaderFooter/DropdownProfile.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from 'react-dropdown';
+import { NavLink } from 'react-router-dom';
 
-
+// Mal for å finne ut av dropdown for sidepanel
 const DropdownProfile = () => {
     const options =[
-        {label: "Mine Vurderinger", value: "myReviews"},
-        {label: "Logg ut", value: "logout"}
+        {label: "Mine Vurderinger", value: "/myReviews"},
+        {label: "Logg ut", value: "/logout"}
 
     ];
 
-    const [value,, setValue] = React.useState("")
+    const [value, setValue] = useState(null)
 
-    const handleChange = (event) => {setValue(event.target.value);};
+    const LogOut = (e) => {
+        e.preventDefault()
+        localStorage.setItem("loggedIn", "")
+        window.location.reload(false)
+    }
+
+    const handleChange = (selectedOptions) => {setValue(selectedOptions.value);
+    if (setValue(selectedOptions.value) === "/logout"){
+        LogOut();
+    }
+};
 	
+ 
         
     return(
         <div id="mainDropdown">
-           
-            <Dropdown
+           {/*  <button
+            className="toggle"
+    onClick={} => setNavbar/}*/}
+             <Dropdown
             placeholder="##Profilnamn##"
             label="Min profil"
             options={options}
             value={value}
             onChange={handleChange}
-            />
+            />  
 
       </div>  
     )
