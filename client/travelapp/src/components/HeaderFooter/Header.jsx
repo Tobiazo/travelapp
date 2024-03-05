15bcef7789e338b08c2dfef8e1db28ce8614ba74
Sunnivaalvern <sunniva.alvern@hotmail.no>
2024-03-05 09:29:08 +0100

    dropdown bare nar logget inn

--- client/travelapp/src/components/HeaderFooter/Header.jsx
+++ client/travelapp/src/components/HeaderFooter/Header.jsx
@@ -1,37 +1,62 @@
-// Header.js
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import Button from "./Button";
 
-function Header() {
-  const LogOut = (e) => {
-    e.preventDefault()
-    localStorage.setItem("loggedIn", "")
-    window.location.reload(false)
-}
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDropdownOpen: false
+    };
+  }
 
-  return (
-    <div id="header">
-      <div id="linksContainer">
-        <div id="links">
-          <NavLink id="navtohome" to="/">
-            EventyrListen
-          </NavLink>
+  toggleDropdown = () => {
+    console.log("Dropdown toggled");
+    this.setState({ isDropdownOpen: !this.state.isDropdownOpen });
+  }
 
-          <div className="dropdown">
-            <Button className="dropbtn" />
-            <div className="dropdown-content">
-                <NavLink className="dropdownLink" to ="/minevurderinger">{'Mine vurderinger'}  </NavLink><br/>
-                <NavLink className="dropdownLink" to ="/minevurderinger">{'Legg til destinasjon'}  </NavLink><br/>
-                <NavLink className="dropdownLink" id="navlinklogginn" to ="/Login"  onClick={LogOut}>{'Logg ut'}  </NavLink><br/>
-              
-            </div>
+  LogOut = (e) => {
+    e.preventDefault();
+    localStorage.setItem("loggedIn", "");
+    window.location.reload(false);
+  }
+
+  render() {
+    const isLoggedIn = localStorage.getItem("loggedIn"); // Assuming you set this value when the user logs in
+
+    return (
+      <div id="header">
+        <div id="linksContainer">
+          <div id="links">
+            <NavLink id="navtohome" to="/">
+              EventyrListen
+            </NavLink>
+
+            {/*lager logginn/brukernavn-knapp*/}
+            {isLoggedIn ? (
+              <NavLink className="navlink" id="navlinklogginn" onClick={this.toggleDropdown}>
+                Brukernavn
+              </NavLink>
+            ) : (
+              <NavLink className="navlink" id="navlinklogginn" to="/Login">
+                Logg inn
+              </NavLink>
+            )}
+
+            {/* Innholdet i dropdown-menyen */}
+            {isLoggedIn && (
+              <div className="dropdown">
+                <div className="dropdown-content" style={{ display: this.state.isDropdownOpen ? 'block' : 'none' }} id="dropdownContent">
+                  <NavLink className="dropdownLink" to="/minevurderinger">{'Mine vurderinger'}</NavLink><br/>
+                  <NavLink className="dropdownLink" to="/minevurderinger">{'Legg til destinasjon'}</NavLink><br/>
+                  <NavLink className="dropdownLink" id="navlinklogginn" to="/Login" onClick={this.LogOut}>{'Logg ut'}</NavLink><br/>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
-      
-    </div>
-  );
+    );
+  }
 }
 
 export default Header;
