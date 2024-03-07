4dd401e02f48e5d9ab6f527852f2d3073512541b
Eirik Waatevik Saure <eirikwsa@stud.ntnu.no>
2024-03-07 13:08:17 +0100

    Resolve "Utvide side for å legge til reisedestinasjoner"

--- server/models/destinationModel.js
+++ server/models/destinationModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const destinationSchema = new mongoose.Schema({
   destination_name: { type: String, required: true },
   destination_contry: { type: String, required: true},
+  destination_continent: {type:String, required: true},
   ShortDescription: { type: String, required: true},
   category: { type:[String], required: false}, 
   rating: {type: Number, required: false}, 
