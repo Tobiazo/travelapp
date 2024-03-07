2265128e4a77415b296fb6ac13d4bc2b90364c16
Markus Skauvik Veblungsnes <markuv@stud.ntnu.no>
2024-03-07 14:42:31 +0100

    Resolve "Lage funksjonalitet for filtrering og sortering"

--- client/travelapp/src/components/HeaderFooter/Sidebar.jsx
+++ client/travelapp/src/components/HeaderFooter/Sidebar.jsx
@@ -2,54 +2,86 @@ import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import ContinuousSlider from "./ContinuousSlider";
 import TempSlider from "./Tempslider";
+import { useFilter } from "../FilterProvider";
 
-function Sidebar() {
-  return (
-    <div id="sidebar">
-      <p class="links">
-        {" "}
-        <NavLink to="/noSidebar"> &gt; </NavLink>FILTER
-      </p>
-      <div id="Besøkt/Ubesøkt">
-        <label> Besøkt/Ubesøkt </label>
-        <select name="Besøkte" id="">
-          <option value="Vis Begge">Vis Begge</option>
-          <option value="Kun Besøkte">Kun Besøkte</option>
-          <option value="Kun Ubesøkte">Kun Ubesøkte</option>
-        </select>
-      </div>
-      <br />
+function Sidebar(){
+    /* Filter ser slik ut [[ratingMin, RatingMax], [tags], [kontinenter], klima, showVisisted] */
+    const {filter, setFilter} = useFilter()
 
-      <label> Kontinenter: </label>
-      <div id="kontinentCheckboxContainer">
-        <input type="checkbox" /> <label> Europa</label>
-        <br />
-        <input type="checkbox" name="Nord-Amerika" />{" "}
-        <label for="Nord-Amerika"> Nord-Amerika</label>
-        <br />
-        <input type="checkbox" /> <label> Sør-Amerika</label>
-        <br />
-        <input type="checkbox" /> <label> Asia</label>
-        <br />
-        <input type="checkbox" /> <label> Afrika</label>
-        <br />
-        <input type="checkbox" /> <label> Oseania</label>
-      </div>
-      <div id="tagsContainer">
-        <label> Tags </label>
-        <select name="Besøkte" id="">
-          <option value="Ingen">Ingen</option>
-          <option value="Tag1">Sykkling</option>
-        </select>
-      </div>
+    const handleBesøktChange = (event) => {
+        const newFilter = [...filter]
+        newFilter[4] = event.target.value
+        setFilter(newFilter)
+        console.log(filter)
+      };
 
-      <p>⭐Vurdering:</p>
+      const handleKontinenterChange = (event) => {
+        const newFilter = [...filter];
+        const checkedValue = event.target.value;
+        const isChecked = event.target.checked;
+        
+       
+        const index = newFilter[2].indexOf(checkedValue);
+      
+        if (isChecked && index === -1) {
+          newFilter[2].push(checkedValue);
+        } else if (!isChecked && index !== -1) {
 
-      <ContinuousSlider />
-      <p>Temp:</p>
-      <TempSlider />
-    </div>
-  );
+          newFilter[2].splice(index, 1);
+        }
+        setFilter(newFilter);
+      };
+
+    return (
+        <>
+        
+        <div id="sidebar">
+
+           <p class="links"> <NavLink to ="/noSidebar"> &gt; </NavLink>FILTER</p> 
+
+
+           <div id="Besøkt/Ubesøkt">
+           <label> Besøkt/Ubesøkt </label>
+           <select onChange={handleBesøktChange} name="Besøkte" id="" >
+                <option value="Vis Begge">Vis Begge</option>
+                <option value="Kun Besøkte">Kun Besøkte</option>
+                <option value="Kun Ubesøkte">Kun Ubesøkte</option>
+            </select>
+            </div>
+            <br />
+
+            <label> Kontinenter: </label>
+            <div id="kontinentCheckboxContainer" onChange={handleKontinenterChange}>
+                
+                <input type="checkbox" /> <label> Europa</label>
+                <br />
+                <input type="checkbox" name="Nord-Amerika"/> <label for="Nord-Amerika"> Nord-Amerika</label>
+                <br />
+                <input type="checkbox" /> <label> Sør-Amerika</label>
+                <br />
+                <input type="checkbox" /> <label> Asia</label> 
+                <br />
+                <input type="checkbox" /> <label> Afrika</label>
+                <br />
+                <input type="checkbox" /> <label> Oceania</label>
+            </div>
+            <div id="tagsContainer">
+                <label> Tags </label>
+                <select name="Besøkte" id="">
+                        <option value="Ingen">Ingen</option>
+                        <option value="Tag1">Sykkling</option>
+                </select>
+            </div>
+
+            <p>⭐Vurdering:</p>
+        
+           <ContinuousSlider/>
+           <p>Temp:</p>
+           <TempSlider/>
+        </div> 
+        </>
+    )
 }
+
 
 export default Sidebar;
--- client/travelapp/src/pages/Home.jsx
+++ client/travelapp/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import LoginButton from "./../components/LoginButton";
 import Destinasjonsboks from "../components/Destinasjonsboks";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useFilter } from '../components/FilterProvider'; 
 
 // const Home = (props) => {
 //   const [traveldestinations, setTraveldestinations] = useState(null);
@@ -33,9 +34,15 @@ import axios from "axios";
 //   }
 
 
-
 const Home = (props) => {
-  const [traveldestinations, setTravelDestinations] = useState(null);
+const [traveldestinations, setTraveldestinations] = useState(null);
+  
+  const [user, setUser] = useState(null);
+  
+  const {filter, setFilter} = useFilter()
+
+ 
+  //const navigate = useNavigate();
 
   useEffect(() => {
     const fetchTravelDestinations = async () => {
@@ -50,12 +57,28 @@ const Home = (props) => {
           return { ...traveldestination, averageRating: Math.round(averageJson.averageRating*100)/100 };
         }));
 
-        setTravelDestinations(destinationsWithAverage);
+        setTraveldestinations(destinationsWithAverage);
       }
     };
 
     fetchTravelDestinations();
   }, []);
+
+  function setVisit() {
+
+  }
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch("http://localhost:4000/api/users/find/" + localStorage.getItem("loggedIn"));
+
+      const json = await response.json();
+      if (response.ok) {
+        setUser(json);
+      }
+    };
+
+    fetchUser();
+  });
 
   return (
     <div>
@@ -83,10 +106,79 @@ const Home = (props) => {
                 <Destinasjonsboks land={"Toronto"} tittel={"Toronto"} beskrivelse={"Beverenes hjemby. Og mest folkerike byen i canada. Kjent for deres hyggelige tilnærming"} />    
                 </div> */}
         {/* {loggedIn ? <div>Your email address is {email}</div> : <div />} */}
+
+        
+        setFilterCriterion([])
+
+
+
+
       </div>
       <div className="Traveldestinations">
+
+        {/*Formatet for filtering per nå:
+          Filter ser slik ut [[ratingMin, RatingMax], [tags], [kontinenter], klima, showVisisted]
+          dersom kontinenter er full skal den sortes som full
+        */}
+
         {traveldestinations &&
-          traveldestinations.map((traveldestination) => (
+          traveldestinations.filter(ele =>{ //filtrering av Rating
+            return  filter[0][0] <= ele.rating && ele.rating <= filter[0][1]
+          }).
+          filter(ele =>{ //filtrering av Besøkt/ikke Besøkt
+            
+            if (filter[4] == "Vis Begge"){
+              return true
+            }
+            else if (user == null){
+              return true
+            }
+            else if (filter[4] == "Kun Besøkte"){
+              const visitedArray = user.destinations
+              for (let index = 0; index < visitedArray.length; index++) {
+                const element = visitedArray[index];
+                 if(element.destinationId == ele._id){
+                  console.log(element.hasVisited)
+
+                  return element.hasVisited
+                }
+              }
+              return  false
+            }
+            else if (filter[4] == "Kun Ubesøkte"){
+              const visitedArray = user.destinations
+
+              for (let index = 0; index < visitedArray.length; index++) {
+                const element = visitedArray[index];
+                 if(element.destinationId == ele._id){
+                  console.log(element.hasVisited)
+
+                  return !element.hasVisited
+                }
+              }
+              return  true
+            }
+          }).
+          
+          filter(ele =>{            //filtrering av kontinent
+            if(filter[2].length == 0){
+              return true
+            }
+            else{
+              return filter[2].indexOf(ele.destination_continent) != -1
+            }
+          }).
+
+          /* filter(ele =>{            //filtrering av tags
+            if(filter[1].length == 0){
+              return true
+            }
+            else{
+              return filter[1].indexOf(ele.destination_continent) != -1
+            }
+          }). */
+
+          map((traveldestination) => (
             <Destinasjonsboks
               key={traveldestination._id}
               id={traveldestination._id}
