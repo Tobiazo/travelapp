9f4490e8f2728fc196a94971f6f3b39c90975557
hjalmabj <hjalmabj@stud.ntnu.no>
2024-03-21 09:38:07 +0100

    Css i sidebar for anbef destinations

--- client/travelapp/src/components/HeaderFooter/Sidebar.jsx
+++ client/travelapp/src/components/HeaderFooter/Sidebar.jsx
@@ -102,7 +102,7 @@ function Sidebar() {
         <br />
          {localStorage.getItem("loggedIn") && (
         <div id="toggle-anbefalinger">
-          <label>Anbefalte destinasjoner</label>
+          <label id="anbefalt-dest-label" >Anbefalte destinasjoner</label>
           <select onChange={handleAnbefalingerChange} name="" id="">
             <option value="Vis anbefalinger">Vis anbefalinger</option>
             <option value="Skjul anbefalinger">Skjul anbefalinger</option>
